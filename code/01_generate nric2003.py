45398ced65ff99976c53fbb60b6ac7f29d7696db
Bryan <hongyime@users.noreply.github.com>
2020-08-07 14:13:34 +0800

    Add files via upload

---
+++ code/01_generate nric2003.py
@@ -0,0 +1,134 @@
+def nric2003():
+    for i in range (0,99999):
+        prefix = "T03"
+        numbers = str(i).zfill(5)
+        suffix = "A"
+        print(prefix+numbers+suffix)
+    for i in range (0,99999):
+        prefix = "T03"
+        numbers = str(i).zfill(5)
+        suffix = "B"
+        print(prefix+numbers+suffix)
+    for i in range (0,99999):
+        prefix = "T03"
+        numbers = str(i).zfill(5)
+        suffix = "C"
+        print(prefix+numbers+suffix)
+    for i in range (0,99999):
+        prefix = "T03"
+        numbers = str(i).zfill(5)
+        suffix = "D"
+        print(prefix+numbers+suffix)
+    for i in range (0,99999):
+        prefix = "T03"
+        numbers = str(i).zfill(5)
+        suffix = "E"
+        print(prefix+numbers+suffix)
+    for i in range (0,99999):
+        prefix = "T03"
+        numbers = str(i).zfill(5)
+        suffix = "F"
+        print(prefix+numbers+suffix)
+    for i in range (0,99999):
+        prefix = "T03"
+        numbers = str(i).zfill(5)
+        suffix = "G"
+        print(prefix+numbers+suffix)
+    for i in range (0,99999):
+        prefix = "T03"
+        numbers = str(i).zfill(5)
+        suffix = "H"
+        print(prefix+numbers+suffix)
+    for i in range (0,99999):
+        prefix = "T03"
+        numbers = str(i).zfill(5)
+        suffix = "I"
+        print(prefix+numbers+suffix)
+    for i in range (0,99999):
+        prefix = "T03"
+        numbers = str(i).zfill(5)
+        suffix = "J"
+        print(prefix+numbers+suffix)
+    for i in range (0,99999):
+        prefix = "T03"
+        numbers = str(i).zfill(5)
+        suffix = "K"
+        print(prefix+numbers+suffix)
+    for i in range (0,99999):
+        prefix = "T03"
+        numbers = str(i).zfill(5)
+        suffix = "L"
+        print(prefix+numbers+suffix)
+    for i in range (0,99999):
+        prefix = "T03"
+        numbers = str(i).zfill(5)
+        suffix = "M"
+        print(prefix+numbers+suffix)
+    for i in range (0,99999):
+        prefix = "T03"
+        numbers = str(i).zfill(5)
+        suffix = "N"
+        print(prefix+numbers+suffix)
+    for i in range (0,99999):
+        prefix = "T03"
+        numbers = str(i).zfill(5)
+        suffix = "O"
+        print(prefix+numbers+suffix)
+    for i in range (0,99999):
+        prefix = "T03"
+        numbers = str(i).zfill(5)
+        suffix = "P"
+        print(prefix+numbers+suffix)
+    for i in range (0,99999):
+        prefix = "T03"
+        numbers = str(i).zfill(5)
+        suffix = "Q"
+        print(prefix+numbers+suffix)
+    for i in range (0,99999):
+        prefix = "T03"
+        numbers = str(i).zfill(5)
+        suffix = "R"
+        print(prefix+numbers+suffix)
+    for i in range (0,99999):
+        prefix = "T03"
+        numbers = str(i).zfill(5)
+        suffix = "S"
+        print(prefix+numbers+suffix)
+    for i in range (0,99999):
+        prefix = "T03"
+        numbers = str(i).zfill(5)
+        suffix = "T"
+        print(prefix+numbers+suffix)
+    for i in range (0,99999):
+        prefix = "T03"
+        numbers = str(i).zfill(5)
+        suffix = "U"
+        print(prefix+numbers+suffix)
+    for i in range (0,99999):
+        prefix = "T03"
+        numbers = str(i).zfill(5)
+        suffix = "V"
+        print(prefix+numbers+suffix)
+    for i in range (0,99999):
+        prefix = "T03"
+        numbers = str(i).zfill(5)
+        suffix = "W"
+        print(prefix+numbers+suffix)
+    for i in range (0,99999):
+        prefix = "T03"
+        numbers = str(i).zfill(5)
+        suffix = "X"
+        print(prefix+numbers+suffix)
+    for i in range (0,99999):
+        prefix = "T03"
+        numbers = str(i).zfill(5)
+        suffix = "Y"
+        print(prefix+numbers+suffix)
+    for i in range (0,99999):
+        prefix = "T03"
+        numbers = str(i).zfill(5)
+        suffix = "Z"
+        print(prefix+numbers+suffix)
+
+
+nric2003()
