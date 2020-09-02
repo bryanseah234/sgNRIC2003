f96738fea537b3c69fb77e0857f86329c723440f
Bryan <hongyime@users.noreply.github.com>
2020-09-02 18:23:06 +0800

    Update 03_generate barcodes.py

--- code/03_generate barcodes.py
+++ code/03_generate barcodes.py
@@ -2,9 +2,9 @@ import barcode
 from barcode.writer import ImageWriter
 
 
-with open('C:\\Users\\your name\\Downloads\\validated nric2003.txt', 'r') as f:
+with open('C:\\Users\\yourname\\Downloads\\validated nric2003.txt', 'r') as f:
     for i in f:
         i = i.strip('\n')
-        code39 = barcode.get('code39', i, writer=ImageWriter())
+        code39 = barcode.get('code39', i, writer=ImageWriter(), add_checksum=False)
         filename = code39.save(i)
 
